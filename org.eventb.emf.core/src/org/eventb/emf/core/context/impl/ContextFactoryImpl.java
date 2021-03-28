1032b422b2346083e0e88f3f4b95fcbc1403395e
cfsnook <cfs@soton.ac.uk>
2021-03-28 10:28:55 +0100

    remove auto generated import

--- org.eventb.emf.core/src/org/eventb/emf/core/context/impl/ContextFactoryImpl.java
+++ org.eventb.emf.core/src/org/eventb/emf/core/context/impl/ContextFactoryImpl.java
@@ -15,7 +15,6 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.impl.EFactoryImpl;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
-import org.eventb.emf.core.context.*;
 import org.eventb.emf.core.context.Axiom;
 import org.eventb.emf.core.context.CarrierSet;
 import org.eventb.emf.core.context.Constant;
