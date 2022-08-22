baaeadd732328615fba269dbd07596e701512269
cfsnook <cfs@soton.ac.uk>
2022-08-22 15:35:25 +0100

    Manually reviewed and fixed copyright headers..
    (some initial dates were still wrong)

--- org.eventb.emf.core/src/org/eventb/emf/core/context/ContextFactory.java
+++ org.eventb.emf.core/src/org/eventb/emf/core/context/ContextFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 University of Southampton, Heinrich-Heine University Dusseldorf and others.
+ * Copyright (c) 2006 University of Southampton, Heinrich-Heine University Dusseldorf and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
